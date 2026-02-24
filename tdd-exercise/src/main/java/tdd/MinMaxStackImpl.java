5812af5eadd39b8ba99a39d2717d88cdc71f54bc
TorioCrema <simone.zama@studio.unibo.it>
2026-02-24 15:25:02 +0100

    feat: improve MinMaxStackImpl readability

--- tdd-exercise/src/main/java/tdd/MinMaxStackImpl.java
+++ tdd-exercise/src/main/java/tdd/MinMaxStackImpl.java
@@ -17,45 +17,33 @@ public class MinMaxStackImpl implements MinMaxStack {
 
     @Override
     public int pop() {
-        if (this.stack.isEmpty()) {
-            throw new IllegalStateException("Cannot pop when empty.");
-        } else {
-            final int popped = this.stack.remove(this.stack.size() - 1);
-            if (popped == this.max) {
-                this.max = this.stack.stream().max((x, y) -> x > y ? x : y).orElseGet(() -> this.max);
-            }
-            if (popped == this.min) {
-                this.min = this.stack.stream().min((x, y) -> x > y ? y : x).orElseGet(() -> this.min);
-            }
-            return popped;
+        this.throwIfEmpty("Cannot pop when empty.");
+        final int popped = this.stack.remove(this.stack.size() - 1);
+        if (popped == this.max) {
+            this.max = this.stack.stream().max((x, y) -> x > y ? x : y).orElseGet(() -> this.max);
         }
+        if (popped == this.min) {
+            this.min = this.stack.stream().min((x, y) -> x > y ? y : x).orElseGet(() -> this.min);
+        }
+        return popped;
     }
 
     @Override
     public int peek() {
-        if (this.stack.isEmpty()) {
-            throw new IllegalStateException("Cannot peek when empty.");
-        } else {
-            return this.stack.get(this.stack.size() - 1);
-        }
+        this.throwIfEmpty("Cannot peek when empty.");
+        return this.stack.get(this.stack.size() - 1);
     }
 
     @Override
     public int getMin() {
-        if (this.isEmpty()) {
-            throw new IllegalStateException("Cannot get minimum value when empty.");
-        } else {
-            return this.min;
-        }
+        this.throwIfEmpty("Cannot get minimum value when empty.");
+        return this.min;
     }
 
     @Override
     public int getMax() {
-        if (this.isEmpty()) {
-            throw new IllegalStateException("Cannot get maximum value when empty.");
-        } else {
-            return this.max;
-        }
+        this.throwIfEmpty("Cannot get maximum value when empty.");
+        return this.max;
     }
 
     @Override
@@ -66,5 +54,11 @@ public class MinMaxStackImpl implements MinMaxStack {
     @Override
     public int size() {
         return this.stack.size();
+    }
+
+    private void throwIfEmpty(final String msg) {
+        if (this.isEmpty()) {
+            throw new IllegalStateException(msg);
+        }
     }
 }
