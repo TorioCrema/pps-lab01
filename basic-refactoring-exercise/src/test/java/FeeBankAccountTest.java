b6bff30d20047d553bdf5b06f8a225c1abbe432d
TorioCrema <simone.zama@studio.unibo.it>
2026-02-24 11:31:41 +0100

    feat: FeeBankAccount testCorrectDeposit & test refactoring

--- basic-refactoring-exercise/src/test/java/FeeBankAccountTest.java
+++ basic-refactoring-exercise/src/test/java/FeeBankAccountTest.java
@@ -1,6 +1,7 @@
 import example.model.AccountHolder;
 import example.model.BankAccount;
 import example.model.FeeBankAccount;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,10 +10,21 @@ public class FeeBankAccountTest {
     private BankAccount bankAccount;
     private AccountHolder accountHolder;
 
-    @Test
-    public void testInialBalance() {
+    @BeforeEach
+    public void beforeEach() {
         this.accountHolder = new AccountHolder("Mario", "Rossi", 1);
         this.bankAccount = new FeeBankAccount(this.accountHolder, 0);
+    }
+
+    @Test
+    public void testInialBalance() {
         assertEquals(0, this.bankAccount.getBalance());
+    }
+
+    @Test
+    public void testCorrectDeposit() {
+        final double depositAmount = 100;
+        this.bankAccount.deposit(this.accountHolder.id(), depositAmount);
+        assertEquals(depositAmount, this.bankAccount.getBalance());
     }
 }
