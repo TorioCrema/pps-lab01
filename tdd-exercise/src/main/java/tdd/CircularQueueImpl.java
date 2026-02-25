c4762c63692b21b37cb4a176c8100399dae86b81
TorioCrema <simone.zama@studio.unibo.it>
2026-02-25 10:53:57 +0100

    refactor: CircularQueueImpl remove constructor

--- tdd-exercise/src/main/java/tdd/CircularQueueImpl.java
+++ tdd-exercise/src/main/java/tdd/CircularQueueImpl.java
@@ -4,23 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CircularQueueImpl implements CircularQueue {
-    private static final int DEFAULT_CAPACITY = 5;
 
-    private final int capacity;
+    private final int capacity = 5;
     private final List<Integer> list = new ArrayList<>();
-
-    /**
-     * Creates a circular queue with given maximum capacity.
-     *
-     * @param capacity The maximum capacity of the queue.
-     */
-    public CircularQueueImpl(final int capacity) {
-        this.capacity = capacity;
-    }
-
-    public CircularQueueImpl() {
-        this(DEFAULT_CAPACITY);
-    }
 
     @Override
     public boolean isEmpty() {
